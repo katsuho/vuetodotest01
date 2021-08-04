ac048246f0f1280aceb1dd52ff71e16b9e019dc7
k_houzouji <k_houzouji@sdcns.co.jp>
2021-08-04 09:26:30 +0900

    [追加]初期

---
+++ docs/precache-manifest.30443690f5c776c42b5cf6911cef15c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b06e669c59710fa5d2",
+    "url": "/vuetest05/css/app.50db9c19.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vuetest05/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a0329726bb660b7a210d1eb7fa6767f9",
+    "url": "/vuetest05/index.html"
+  },
+  {
+    "revision": "67b06e669c59710fa5d2",
+    "url": "/vuetest05/js/app.08bd92af.js"
+  },
+  {
+    "revision": "aece0511eef5f47af459",
+    "url": "/vuetest05/js/chunk-vendors.c1f1e1ad.js"
+  },
+  {
+    "revision": "7a5ad07fa9f4f86567830de3b0064f5e",
+    "url": "/vuetest05/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuetest05/robots.txt"
+  }
+]);
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  publicPath: "/vuetest05/", // GitHubPagesにあげるならリポジトリ名をいれる,
+  assetsDir: "",
+  outputDir: "docs/", // GitHubPagesにあげるならこれをする方がいい
+}
